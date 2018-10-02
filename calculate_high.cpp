69618583bae2d1ece89ed50c5b39e146a183a192
wuyuzhiji <34713201+wuyuzhiji@users.noreply.github.com>
2018-10-02 16:12:17 +0800

    Update calculate_high.cpp

--- calculate_high.cpp
+++ calculate_high.cpp
@@ -12,111 +12,196 @@ using namespace std;
 #define lson rt<<1
 #define rson lson|
 using namespace std;
+
+
+/*
+sin:a
+cos:c
+tan:t
+asin:S
+acos:C
+atan:T
+pow:^
+ln:l
+优先级：+ - < 函数 < * / < ^
+*/
+const string pi="3.1415926535897932384626433 ";
 string to_suf(string s)
 {
-    stack<char>q;
-    s = '(' + s + ')';
-    string res;
-    int len = s.size();
-    for (int i = 0; i < len; ++i)
-    {
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            if ((s[i - 1] == '+' || s[i - 1] == '-') && s[i - 2] == '(')
-            {
-                res += "0 ";
-                q.push(s[i - 1]);
-            }
-            while (s[i] >= '0' && s[i] <= '9')
-            {
-                res += s[i];
-                ++i;
-            }
-            res += ' ';
-            --i;
-        }
-        else if (s[i] == '(')
-            q.push(s[i]);
-        else if (s[i] == ')')
-        {
-            while (q.top() != '(')
-            {
-                res += q.top();
-                q.pop();
-            }
-            q.pop();
-        }
-        else
-        {
-            if (s[i] == '+' || s[i] == '-')
-            {
-                if (s[i - 1] == '(')
-                    continue;
-                while (q.top() != '(')
-                {
-                    res += q.top();
-                    q.pop();
-                }
-                q.push(s[i]);
-            }
-            else
-            {
-                while (q.top() == '*' || q.top() == '/')
-                {
-                    res += q.top();
-                    q.pop();
-                }
-                q.push(s[i]);
-            }
-        }
-    }
-    while (!q.empty())
-    {
-        res += q.top();
-        q.pop();
-    }
-    return res;
+	string res;
+	s='('+s+')';
+	stack<char>q;
+	int len=s.size();
+	for(int i=0;i<len;++i)
+	{
+        if(s[i]=='p')
+            res+=pi;
+		else if(s[i]>='0'&&s[i]<='9')
+		{
+			if((s[i-1]=='+'||s[i-1]=='-')&&s[i-2]=='(')
+			{
+				res+="0 ";
+				q.push(s[i-1]);
+			}
+			while((s[i]>='0'&&s[i]<='9')||s[i]=='.')
+			{
+				res+=s[i];
+				++i;
+			}
+			res+=' ';
+			--i;
+		}
+		else if(s[i]=='(')
+			q.push(s[i]);
+		else if(s[i]==')')
+		{
+			while(q.top()!='(')
+			{
+				res+=q.top();
+				q.pop();
+			}
+			q.pop();
+		}
+		else if(s[i]=='+'||s[i]=='-')
+		{
+			if(s[i-1]=='(')
+				continue;
+			while(!q.empty()&&q.top()!='(')
+			{
+				res+=q.top();
+				q.pop();
+			}
+			q.push(s[i]);
+		}
+		else if(s[i]>='a'&&s[i]<='z')
+		{
+			while(!q.empty()&&q.top()!='('&&q.top()!='+'&&q.top()!='-')
+			{
+				res+=q.top();
+				q.pop();
+			}
+			if(s[i]=='a')
+			{
+				++i;
+				q.push(s[i]+'A'-'a');
+				i+=2;
+			}
+			else if(s[i]=='l')
+			{
+				++i;
+				q.push('l');
+			}
+			else
+			{
+				q.push(s[i]);
+				i+=2;
+			}
+		}
+		else if(s[i]=='*'||s[i]=='/')
+		{
+			while(!q.empty()&&(q.top()=='*'||q.top()=='/'||q.top()=='^'))
+			{
+				res+=q.top();
+				q.pop();
+			}
+			q.push(s[i]);
+		}
+		else if(s[i]=='^')
+		{
+			while(!q.empty()&&q.top()=='^')
+			{
+				res+=q.top();
+				q.pop();
+			}
+			q.push(s[i]);
+		}
+	}
+	while(!q.empty())
+	{
+		res+=q.top();
+		q.pop();
+	}
+	return res;
 }
-stack<int>num;
+stack<double>num;
 int main()
 {
-    string s;
-    while (cin >> s)
-    {
-        s = to_suf(s);
-        int len = s.size();
-        for (int i = 0; i < len; ++i)
-        {
-            if (s[i] >= '0' && s[i] <= '9')
-            {
-                int val = 0;
-                while (s[i] >= '0' && s[i] <= '9')
-                {
-                    val += s[i] - '0';
-                    val *= 10;
-                    ++i;
-                }
-                val /= 10;
-                num.push(val);
-            }
-            else
-            {
-                int r = num.top();
-                num.pop();
-                int l = num.top();
-                num.pop();
-                if (s[i] == '+')
-                    num.push(l + r);
-                else if (s[i] == '-')
-                    num.push(l - r);
-                else if (s[i] == '*')
-                    num.push(l * r);
-                else if (s[i] == '/')
-                    num.push(l / r);
-            }
-        }
-        cout << num.top() << endl;
-        num.pop();
-    }
-}
+	string s;
+	while(cin>>s)
+	{
+		s=to_suf(s);
+		int len=s.size();
+		for(int i=0;i<len;++i)
+		{
+			if(s[i]>='0'&&s[i]<='9')
+			{
+				double val1=0,val2=0;
+				while(s[i]>='0'&&s[i]<='9')
+				{
+					val1+=s[i]-'0';
+					val1*=10;
+					++i;
+				}
+				val1/=10;
+				if(s[i]=='.')
+				{
+					++i;
+					while(s[i]>='0'&&s[i]<='9')
+					{
+						val2+=s[i]-'0';
+						val2*=10;
+						++i;
+					}
+					while(val2>=1)
+						val2/=10;
+				}
+				num.push(val1+val2);
+			}
+			else
+			{
+				double r=num.top();
+				num.pop();
+				if(s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/'||s[i]=='^')
+				{
+					double l=num.top();
+					num.pop();
+					double ans;
+					if(s[i]=='+')
+						ans=l+r;
+					else if(s[i]=='-')
+						ans=l-r;
+					else if(s[i]=='*')
+						ans=l*r;
+					else if(s[i]=='/')
+						ans=l/r;
+					else if(s[i]=='^')
+						ans=pow(l,r);
+					num.push(ans);
 
+				}
+				else
+				{
+					double ans;
+					if(s[i]=='s')
+						ans=sin(r);
+					else if(s[i]=='c')
+						ans=cos(r);
+					else if(s[i]=='t')
+						ans=tan(r);
+					else if(s[i]=='S')
+						ans=asin(r);
+					else if(s[i]=='C')
+						ans=acos(r);
+					else if(s[i]=='T')
+						ans=atan(r);
+					else if(s[i]=='l')
+						ans=log(r);
+					num.push(ans);
+				}
+			}
+		}
+		cout<<num.top()<<endl;
+		num.pop();
+	}
+}
+					
