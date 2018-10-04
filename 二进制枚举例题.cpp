4d7a455fc80a20f5fc0f48c781c9ce56331a1245
sweet_fish <34713201+wuyuzhiji@users.noreply.github.com>
2018-10-04 16:18:34 +0800

    Update 二进制枚举例题.cpp

--- 二进制枚举例题.cpp
+++ 二进制枚举例题.cpp
@@ -1,4 +1,12 @@
-for (int i = 0; i < (1 << 8); ++i)
+int main()
 {
-	//i即为枚举出的状态，接下来写个check函数检验即可
+	int ans = 0;
+	for (int i = 0; i < (1 << 8); ++i)
+	{
+		//i即为枚举出的状态，接下来写个check函数检验即可
+		/*
+		if(check(i))
+			++ans;
+		*/
+	}
 }
