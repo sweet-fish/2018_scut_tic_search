c5cbd04efb5591e76ea047184a3608c18c81a085
sweet_fish <34713201+wuyuzhiji@users.noreply.github.com>
2018-10-04 16:27:38 +0800

    Update 二进制枚举例题.cpp

--- 二进制枚举例题.cpp
+++ 二进制枚举例题.cpp
@@ -1,14 +1,13 @@
-int main()
+bool check(int i)
 {
-	int ans = 0;
-	for (int i = 0; i < (1 << 8); ++i)
+	int cnt = 0;
+	while (i)//逐位检查i的二进制表达
 	{
-		/*
-		i即为枚举出的状态
-		接下来写个check函数检验即可
-		*/
-		if (check(i))
-			++ans;
+		if (i & 1)
+			++cnt;
+		i >>= 1;
 	}
-	cout << ans << endl;
+	if (cnt == 2)
+		return true;
+	return false;
 }
