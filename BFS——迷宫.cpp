70966e8146a679c59bde133610915e4054a298f9
sweet_fish <34713201+wuyuzhiji@users.noreply.github.com>
2018-10-06 01:40:37 +0800

    Update BFS——迷宫.cpp

--- BFS——迷宫.cpp
+++ BFS——迷宫.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include <queue>
-char maze[50][50];
-bool vis[50][50];
+#include<queue>
+using namespace std;
+char maze[50][50];//储存迷宫
+int vis[50][50];//到达该坐标的最少耗时
+//bool vis[50][50];代表是否已经经过该点
 const int xi[] = { 1,-1,0,0 };
-const int yi[] = { 0,0,1,-1 };
+const int yi[] = { 0,0,1,-1 };//行走方向
 struct p//表达状态的结构体
-{
-	int x, y, t;
-};
+{int x, y, t;};
 queue<p>bfs;//按顺序存放状态的队列
 int main()
 {
-	memset(vis, 0, sizeof(vis));
 	int n, m;
 	cin >> n >> m;
+	memset(vis, 0, sizeof(vis));
 	for (int i = 0; i < n; ++i)
 		for (int j = 0; j < m; ++j)
 		{
@@ -30,9 +30,12 @@ int main()
 		{
 			int nx = now.x + xi[i], ny = now.y + yi[i], nt = now.t + 1;
 
-			if (vis[nx][ny])	//跳过之前已经走过的点
+			//该点有比现在更快到达的方法，跳过			该点更新为已到
+			if (vis[nx][ny] && vis[nx][ny] <= nt)	//if(vis[nx][ny])
 				continue;
-			vis[nx][ny] = 1;
+
+			//更新该点到达时间		更新该点为到过
+			vis[nx][ny] = nt;	//vis[nx][ny] = 1;
 
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m || maze[nx][ny] == '#')
 				continue;		//排除不合法情况
