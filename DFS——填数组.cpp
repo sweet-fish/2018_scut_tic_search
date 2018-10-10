e27c3e1d9ca57f5d95ff848ad4139ca585af4970
sweet_fish <34713201+wuyuzhiji@users.noreply.github.com>
2018-10-11 02:00:00 +0800

    Update DFS——填数组.cpp

--- DFS——填数组.cpp
+++ DFS——填数组.cpp
@@ -1,14 +1,33 @@
+#include <iostream>
+using namespace std;
 int a[2][2];
-	for(a[0][0]=1;a[0][0]<=3;++a[0][0])
-		for (a[0][1] = 1; a[0][1] <= 3; ++a[0][1])
-			for (a[1][0] = 1; a[1][0] <= 3; ++a[1][0])
-				for (a[1][1] = 1; a[1][1] <= 3; ++a[1][1])
-				{
-					for (int i = 0; i < 2; ++i)
-					{
-						for (int j = 0; j < 2; ++j)
-							cout << a[i][j];
-						cout << endl;
-					}
-					cout << endl;
-				}
+void dfs(int x, int y)//得到坐标为(x, y)的元素状态
+{
+	for (int i = 1; i <= 3; ++i)
+	{
+		a[x][y] = i;	//得到状态
+		//判断是否有下一步的状态，
+		if (y < 1)
+			dfs(x, y + 1);
+		else if (x < 1)
+			dfs(x + 1, 0);	//如果有，就得到下一步的状态
+		
+		//到达分支终点，得到一个需要的状态，输出
+		else if (x == 1 && y == 1)
+		{
+			for (int i = 0; i < 2; ++i)
+			{
+				for (int j = 0; j < 2; ++j)
+					cout << a[i][j];
+				cout << endl;
+			}
+			cout << endl;
+		}
+	}
+}
+int main()
+{
+	dfs(0, 0);//从初始状态开始搜索即可
+	return 0;
+}
+
